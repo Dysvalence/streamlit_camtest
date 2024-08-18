d1424446a44a3d479a907064826978e5b76e2a9f
Dysvalence <Dysvalence@users.noreply.github.com>
2024-08-18 09:29:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈testing that camera function")
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    img = Image.open(img_file_buffer)
+    img = np.array(img)
+    st.image(img)
+
+st.write("supposedly this does something")
